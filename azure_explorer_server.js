064f68f39e4fc14c70ff374b14b0bafbdec80c06
Alyssa Williams <alyssaw90@gmail.com>
2016-07-05 14:26:18 -0700

    pulling root directories

--- azure_explorer_server.js
+++ azure_explorer_server.js
@@ -8,8 +8,12 @@ app.set('view engine', 'ejs');
 app.get('/viewer', function(req, res, next) {
   res.render('viewer');
   //res.send(200, 'OK');
+  fs.readdir('/', function(err,data){
+    if (err) throw err;
+    console.log(data);
+  })
 });
 
 var server = app.listen(80);
 
-io = require('socket.io').listen(server); 
+io = require('socket.io').listen(server);
---
+++ views/scripts/test.js
@@ -0,0 +1,3 @@
+document.ready(function(){
+  console.log('script file loading');
+});
