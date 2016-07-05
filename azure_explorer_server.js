12b8c27f774b11978cadabdeb77cdf2f8c4fdbf0
Alyssa Williams <alyssaw90@gmail.com>
2016-07-05 15:32:42 -0700

    testing array pushing

--- azure_explorer_server.js
+++ azure_explorer_server.js
@@ -9,8 +9,12 @@ app.get('/viewer', function(req, res, next) {
 
   //res.send(200, 'OK');
   fs.readdir('/', function(err,data){
+    var allItems = {
+      files: [12000],
+      folders: [124944]
+    };
     if (err) throw err;
-    console.log(data);
+    // console.log(data);
     data.forEach(function(item) {
       fs.stat('/'+ item, function(err, stats){
         if(err) {
@@ -18,13 +22,19 @@ app.get('/viewer', function(req, res, next) {
           return;
         }
         if (stats.isFile()) {
-          console.log("is File")
+          // console.log("is File")
+          console.log(item)
+          allItems['files'].push(item)
         }
         if(stats.isDirectory()){
-          console.log("is Directory")
+          // console.log("is Directory")
+          console.log(item)
+          allItems['folders'].push(item)
         }
       })
     })
+    console.log("Folders: " + allItems.folders);
+    console.log("Files: " + allItems.files);
 
     res.render('viewer', {data:data});
   })
